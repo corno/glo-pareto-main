e363a2008dcb25cf0fa98c703d1d7c900ba50956
corno <corno.astn.a@schraverus.com>
2022-11-28 20:10:31 +0100

    .

--- pub/src/interface/functions/functions.p.ts
+++ pub/src/interface/functions/functions.p.ts
@@ -1,16 +1,16 @@
-import { ISetExitCodeToFailed, IWriteStream } from "../interfaces/interfaces.p"
+import * as pt from "pareto-core-types"
+
+//import { ISetExitCodeToFailed, IWriteStream } from "../interfaces/interfaces.p"
 import { TArguments } from "../types/types.p"
 
 //
 
-export type FProgramMain = (
-    $: {
+export type PProgramMain = pt.Procedure<{
         readonly "arguments": TArguments
-    },
-    $i: {
-        readonly "stdout": IWriteStream
-        readonly "stderr": IWriteStream
-        readonly "setExitCodeToFailed": ISetExitCodeToFailed
     }
-) => void
-
+    // $i: {
+    //     readonly "stdout": IWriteStream
+    //     readonly "stderr": IWriteStream
+    //     readonly "setExitCodeToFailed": ISetExitCodeToFailed
+    // }
+>
